5f1df6e7b6b5b68d7845a3a4637014b33834caa3
Aman Arham <amanarham@gmail.com>
2024-03-12 12:42:49 -0500

    Initial Swift App setup. Easy application!

---
+++ RiskReducationOne/RiskReducationOneUITests/RiskReducationOneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RiskReducationOneUITestsLaunchTests.swift
+//  RiskReducationOneUITests
+//
+//  Created by Aman Arham on 3/12/24.
+//
+
+import XCTest
+
+final class RiskReducationOneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
